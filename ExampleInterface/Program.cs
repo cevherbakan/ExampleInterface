15fe3d7e2da1458b112703ac8f72ffd8f5dca55c
cevherbakan <cevherbakan@gmail.com>
2021-06-09 13:16:26 +0300

    created

---
+++ ExampleInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
